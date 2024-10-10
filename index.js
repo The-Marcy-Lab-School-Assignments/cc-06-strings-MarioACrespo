93f28447d99662a835c5da83555f3108c2a65713
Mario <airbomberofaqua@gmail.com>
2024-10-10 11:01:07 -0400

    done

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 //write your code here
+const reverseString = (str) => {
+  let i = str.length - 1;
+  let tam = "";
+  while (i >= 0) {
+    tam += str[i];
+    i--;
+  }
+  return tam;
+};
+// just  reverse
+console.log(reverseString("motts"));
+
+const reverseZigZagString = (str) => {
+  let i = str.length - 1;
+  let water = "";
+  while (i >= 0) {
+    if (i % 2 === 0) {
+      water += str[i].toUpperCase();
+    } else {
+      water += str[i];
+    }
+    i--;
+  }
+  return water;
+};
+
+console.log(reverseZigZagString("bottle"));
